44989f623f217b681c595ffb3b7158f6ace5d1de
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-24 11:05:26 +0100

    implementar animacion de hover en el header

--- costera-n1/app/components/Header.jsx
+++ costera-n1/app/components/Header.jsx
@@ -7,6 +7,11 @@ export const Header = ({ showText }) => {
 
   const linkClass = (path) => (pathname === path ? "font-bold underline" : "");
 
+  const variantsLink = {
+    initial: { y: 0 },
+    hover: { y: -25 },
+  };
+
   return (
     <>
       <AnimatePresence mode="wait">
@@ -23,22 +28,67 @@ export const Header = ({ showText }) => {
             transition={{ duration: 2, ease: "easeOut", delay: 2 }}
           >
             <ul className="flex gap-4">
-              <li>
-                <Link href="/" className={linkClass("/")}>
-                  Inicio
-                </Link>
+              <li className="h-6 overflow-hidden">
+                <motion.div
+                  variants={variantsLink}
+                  initial="initial"
+                  whileHover="hover"
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className="flex flex-col"
+                >
+                  <Link href="/" className={linkClass("/")}>
+                    Inicio
+                  </Link>
+                  <Link href="/" className={linkClass("/")}>
+                    Inicio
+                  </Link>
+                </motion.div>
               </li>
-              <li>
-                <Link href="/detalles" className={linkClass("/detalles")}>
-                  Detalles
-                </Link>
+
+              <li className="h-6 overflow-hidden">
+                <motion.div
+                  variants={variantsLink}
+                  initial="initial"
+                  whileHover="hover"
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className="flex flex-col"
+                >
+                  <Link href="/detalles" className={linkClass("/detalles")}>
+                    Detalles
+                  </Link>
+                  <Link href="/detalles" className={linkClass("/detalles")}>
+                    Detalles
+                  </Link>
+                </motion.div>
               </li>
-              <li>
-                <Link href="/contacto" className={linkClass("/contacto")}>
-                  Contacto
-                </Link>
+              <li className="h-6 overflow-hidden">
+                <motion.div
+                  variants={variantsLink}
+                  initial="initial"
+                  whileHover="hover"
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className="flex flex-col"
+                >
+                  <Link href="/contacto" className={linkClass("/contacto")}>
+                    Contacto
+                  </Link>
+                  <Link href="/contacto" className={linkClass("/contacto")}>
+                    Contacto
+                  </Link>
+                </motion.div>
               </li>
-              <li>Idioma</li>
+              <li className="h-6 overflow-hidden">
+                <motion.div
+                  variants={variantsLink}
+                  initial="initial"
+                  whileHover="hover"
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className="flex flex-col"
+                >
+                  <p>Idioma</p>
+                  <p>Idioma</p>
+                </motion.div>
+              </li>
             </ul>
           </motion.header>
         )}
