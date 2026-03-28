63e9ec012b2934680921d09a648caaf61b3be392
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-28 14:33:04 +0100

    realizar layout en mobile de seccion 1 en HorizontalScroll

--- costera-n1/app/components/HorizontalScroll.jsx
+++ costera-n1/app/components/HorizontalScroll.jsx
@@ -21,6 +21,8 @@ import { SectionZero } from "./SectionZero";
  * Es el que realmente se desliza lateralmente frente a la "cámara" del usuario.
  */
 
+/* ring en Tailwind CSS es como un box-shadow para poder meter un efecto de borde sin ocupar espacio real */
+
 export const HorizontalScroll = () => {
   const targetRef = useRef(null);
 
@@ -29,27 +31,43 @@ export const HorizontalScroll = () => {
   });
 
   // Transformamos el scroll vertical (0 a 1) en movimiento horizontal (-200%)
-  const x = useTransform(scrollYProgress, [0, 1], ["0vw", "-200vw"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["0vw", "-240vw"]);
 
   return (
-    <section ref={targetRef} className="relative h-[300vh] bg-slate-900">
+    <section ref={targetRef} className="relative h-[500vh] bg-slate-900">
       <div className="sticky top-0 flex h-screen items-center overflow-hidden">
         <motion.div style={{ x }} className="flex">
           {/* SECCIÓN 1: El texto que ya tenemos */}
           <SectionZero />
 
           {/* SECCIÓN 2: Galería o fotos */}
-          <div className="w-screen h-screen shrink-0 flex items-center justify-center bg-blue-500 rounded-l-2xl">
-            <h2 className="text-5xl font-black text-white">
-              Fachadas de Colores
-            </h2>
+          <div className=" w-[120vw] h-screen shrink-0 flex flex-col items-center justify-between bg-blue-300 rounded-l-2xl shadow-2xl pt-24 pr-20 pl-10">
+            <div className=" flex flex-col justify-center items-center gap-20">
+              <h3 className="text-lg font-black text-white text-balance">
+                01. El Corazón del Color: Costera de la Mar
+              </h3>
+              <div className="flex flex-col gap-4">
+                <h2 className="text-4xl lg:text-5xl font-black text-white text-pretty">
+                  Vive en el icono de la Costa Blanca
+                </h2>
+                <p className="text-balance">
+                  Te alojas en la Costera de la Mar, la calle más fotografiada y
+                  vibrante de Villajoyosa. Este eje histórico no es solo una
+                  dirección, es el puente que une el legado de la ciudad
+                  amurallada con el espíritu marinero. Despierta rodeado de
+                  fachadas de colores y la brisa del Mediterráneo a solo unos
+                  pasos de tu puerta.
+                </p>
+              </div>
+            </div>
+            <Image src="/hotel-fachada.webp" width={1000} height={800} alt="" />
           </div>
 
           {/* SECCIÓN 3: Detalles finales */}
-          <div className="relative w-screen h-screen shrink-0 flex items-center justify-center bg-orange-500 ">
+          <div className="relative w-[120vw] h-screen shrink-0 flex items-center justify-center bg-orange-300 shadow-2xl">
             <div
               aria-label="hidden "
-              className="absolute inset-y-0 left-0 w-8 -translate-x-8/12 bg-orange-500 z-10 rounded-l-2xl"
+              className="absolute inset-y-0 left-0 w-10 -translate-x-full ring-1 ring-orange-300  bg-orange-300 z-10 rounded-l-2xl"
             />
             <h2 className="text-5xl font-black text-white">Cerca del Mar</h2>
           </div>
