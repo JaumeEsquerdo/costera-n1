719327f8dbdc75488a45520e2cd14be643093018
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-04-01 16:59:13 +0200

    Update BookingCTA.jsx

--- costera-n1/app/components/BookingCTA.jsx
+++ costera-n1/app/components/BookingCTA.jsx
@@ -2,19 +2,20 @@ import { Footer } from "./Footer";
 
 export const BookingCTA = () => {
   return (
-    <section className="h-screen flex flex-col p-8 pt-26">
-      <div>
-        <h3 className="text-2xl font-bold mb-8 text-center">
-          ¿Tienes fechas para tu próxima escapada?
-        </h3>
-        <div className="flex flex-col gap-4 lg:max-w-1/2">
-          <p>
-            Costera N1 te espera en el corazón del casco antiguo. Consulta
-            disponibilidad y reserva de forma segura a través de nuestro gestor
-            oficial.
-          </p>
-          <button>CTA BOOKING</button>
-        </div>
+    <section className="min-h-[60vh] flex flex-col justify-center items-center  p-8 pt-26">
+      <h3 className="text-2xl font-bold mb-8 text-center">
+        ¿Tienes fechas para tu próxima escapada?
+      </h3>
+      <div className="flex flex-col gap-4 items-center lg:max-w-1/2">
+        <p>
+          Costera N1 te espera en el corazón del casco antiguo. Consulta
+          disponibilidad y reserva de forma segura a través de nuestro gestor
+          oficial.
+        </p>
+        <button className="border-2 border-green-950 bg-green-50 rounded-4xl cursor-pointer w-fit py-2 px-6 shadow-[4px_4px_0px_0px_rgba(5,46,7,1)] active:shadow-none active:translate-x-0.5 active:translate-y-0.5 hover:bg-green-100 transition-all">
+          {" "}
+          Ir a página de reservas
+        </button>
       </div>
     </section>
   );
