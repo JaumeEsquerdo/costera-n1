cfc300cddd2d2546db66c161f033a8d85f8b4619
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-04-12 20:32:33 +0200

    actualizar logica de aparecer header con framer motion

--- costera-n1/app/components/Header.jsx
+++ costera-n1/app/components/Header.jsx
@@ -22,121 +22,107 @@ export const Header = ({ showText }) => {
 
   return (
     <>
-      <AnimatePresence mode="wait">
-        {!showText && (
-          <motion.header
-            className="fixed top-10 left-1/2 -translate-x-1/2 z-99 lg:left-1/12 lg:translate-x-0"
-            initial={{ opacity: 0, y: -100 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{
-              opacity: 0,
-              y: -100,
-              transition: { duration: 0.5, ease: "easeIn" },
-            }}
-            transition={{ duration: 1.2, ease: "easeOut", delay: 2 }}
-          >
-            <ul className="relative flex gap-0 px-2 lg:gap-2 lg:px-4">
-              <li className="h-8 overflow-hidden">
-                <motion.div
-                  variants={variantsLink}
-                  initial="initial"
-                  whileHover="hover"
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className={`flex flex-col items-center gap-2 cursor-pointer`}
-                >
-                  <NavElement
-                    href="/"
-                    active={pathname.startsWith(`/${locale}`)}
-                    className="mt-1"
-                  >
-                    INICIO
-                  </NavElement>
-                  <NavElement
-                    href="/"
-                    active={pathname.startsWith(`/${locale}`)}
-                  >
-                    INICIO
-                  </NavElement>
-                </motion.div>
-              </li>
+      <motion.header
+        className="fixed top-10 left-1/2 -translate-x-1/2 z-99 lg:left-1/12 lg:translate-x-0"
+        initial={false}
+        animate={{ opacity: showText ? 0 : 1, y: showText ? -100 : 0 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+      >
+        <ul className="relative flex gap-0 px-2 lg:gap-2 lg:px-4">
+          <li className="h-8 overflow-hidden">
+            <motion.div
+              variants={variantsLink}
+              initial="initial"
+              whileHover="hover"
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className={`flex flex-col items-center gap-2 cursor-pointer`}
+            >
+              <NavElement
+                href="/"
+                active={pathname.startsWith(`/${locale}`)}
+                className="mt-1"
+              >
+                INICIO
+              </NavElement>
+              <NavElement href="/" active={pathname.startsWith(`/${locale}`)}>
+                INICIO
+              </NavElement>
+            </motion.div>
+          </li>
 
-              <li className="h-8 overflow-hidden">
-                <motion.div
-                  variants={variantsLink}
-                  initial="initial"
-                  whileHover="hover"
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="flex flex-col  items-center gap-2 cursor-pointer"
-                >
-                  <NavElement
-                    href="/detalles"
-                    active={pathname.startsWith(`/${locale}/detalles`)}
-                    className="mt-1"
-                  >
-                    DETALLES
-                  </NavElement>
-                  <NavElement
-                    href="/detalles"
-                    active={pathname.startsWith(`/${locale}/detalles`)}
-                  >
-                    DETALLES
-                  </NavElement>
-                </motion.div>
-              </li>
-              <li className="h-8 overflow-hidden">
-                <motion.div
-                  variants={variantsLink}
-                  initial="initial"
-                  whileHover="hover"
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="flex flex-col  items-center gap-2 "
-                >
-                  <NavElement
-                    href="/contacto"
-                    active={pathname.startsWith(`/${locale}/contacto`)}
-                    className="mt-1"
-                  >
-                    CONTACTO
-                  </NavElement>
-                  <NavElement
-                    href="/contacto"
-                    active={pathname.startsWith(`/${locale}/contacto`)}
-                  >
-                    CONTACTO
-                  </NavElement>
-                </motion.div>
-              </li>
-              <li className="h-8 overflow-hidden">
-                <motion.div
-                  variants={variantsLink}
-                  initial="initial"
-                  whileHover="hover"
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                  className="flex flex-col  items-center gap-2 cursor-pointer"
-                >
-                  <NavElement
-                    onClick={handleLangOpen}
-                    as="button"
-                    className="ring-2 mt-1 rounded-full"
-                  >
-                    IDIOMA
-                  </NavElement>
-                  <NavElement
-                    as="button"
-                    onClick={handleLangOpen}
-                    className="ring-2 rounded-full"
-                  >
-                    IDIOMA
-                  </NavElement>
-                </motion.div>
-              </li>
-            </ul>
-            <AnimatePresence>
-              {langIsOpen && <LanguageSwitcher />}
-            </AnimatePresence>
-          </motion.header>
-        )}
-      </AnimatePresence>
+          <li className="h-8 overflow-hidden">
+            <motion.div
+              variants={variantsLink}
+              initial="initial"
+              whileHover="hover"
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="flex flex-col  items-center gap-2 cursor-pointer"
+            >
+              <NavElement
+                href="/detalles"
+                active={pathname.startsWith(`/${locale}/detalles`)}
+                className="mt-1"
+              >
+                DETALLES
+              </NavElement>
+              <NavElement
+                href="/detalles"
+                active={pathname.startsWith(`/${locale}/detalles`)}
+              >
+                DETALLES
+              </NavElement>
+            </motion.div>
+          </li>
+          <li className="h-8 overflow-hidden">
+            <motion.div
+              variants={variantsLink}
+              initial="initial"
+              whileHover="hover"
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="flex flex-col  items-center gap-2 "
+            >
+              <NavElement
+                href="/contacto"
+                active={pathname.startsWith(`/${locale}/contacto`)}
+                className="mt-1"
+              >
+                CONTACTO
+              </NavElement>
+              <NavElement
+                href="/contacto"
+                active={pathname.startsWith(`/${locale}/contacto`)}
+              >
+                CONTACTO
+              </NavElement>
+            </motion.div>
+          </li>
+          <li className="h-8 overflow-hidden">
+            <motion.div
+              variants={variantsLink}
+              initial="initial"
+              whileHover="hover"
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="flex flex-col  items-center gap-2 cursor-pointer"
+            >
+              <NavElement
+                onClick={handleLangOpen}
+                as="button"
+                className="ring-2 mt-1 rounded-full"
+              >
+                IDIOMA
+              </NavElement>
+              <NavElement
+                as="button"
+                onClick={handleLangOpen}
+                className="ring-2 rounded-full"
+              >
+                IDIOMA
+              </NavElement>
+            </motion.div>
+          </li>
+        </ul>
+        <AnimatePresence>{langIsOpen && <LanguageSwitcher />}</AnimatePresence>
+      </motion.header>
     </>
   );
 };
