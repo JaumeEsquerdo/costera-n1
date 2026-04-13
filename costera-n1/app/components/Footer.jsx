c8dd68048b623fbc90dc26f85f28d04fa0e99840
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-04-13 17:40:03 +0200

    añadir traducciones de footer

--- costera-n1/app/components/Footer.jsx
+++ costera-n1/app/components/Footer.jsx
@@ -1,4 +1,10 @@
+import { useI18n } from "../hooks/usei18n";
+
 export const Footer = () => {
+  const { t } = useI18n();
+
+  const textsFooter = t.Index.Footer;
+
   return (
     <footer
       className="relative h-[60vh] bg-green-950 w-full "
@@ -6,9 +12,12 @@ export const Footer = () => {
     >
       <div className="relative h-[calc(100vh+60vh)] -top-[100vh]">
         <div className="sticky top-[calc(100vh-40vh)] h-[40vh] flex flex-col items-center">
-          <h4 className="text-4xl lg:text-9xl text-white font-bold">
-            COSTERETA n1
-          </h4>
+          <div className="flex flex-col gap-4">
+            <h4 className="text-5xl lg:text-9xl text-white font-bold">
+              {textsFooter.title}
+            </h4>
+            <p className="text-center text-white">{textsFooter.subtitle}</p>
+          </div>
         </div>
       </div>
     </footer>
