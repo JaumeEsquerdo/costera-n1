ede6a00e3252f14b778c3ba3d97fa003f3234bf8
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-04-16 13:41:38 +0200

    aplicar layout responsive en mobile de page contacto

--- costera-n1/app/[locale]/contacto/page.jsx
+++ costera-n1/app/[locale]/contacto/page.jsx
@@ -1,11 +1,65 @@
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowTrendUp } from "@fortawesome/free-solid-svg-icons";
 
 export default function Contacto() {
   return (
     <>
       <Header />
-      <main className="min-h-screen">Contenido Contacto</main>
+      <main className="flex flex-col min-h-screen w-[90%] max-w-8xl mx-auto">
+        <div className="pt-40">
+          <h1 className="text-4xl font-title text-center w-full">
+            Contáctanos
+          </h1>
+        </div>
+        <div className="mt-auto py-8 flex flex-col gap-12">
+          <div className="w-full flex flex-col items-center justify-end">
+            <div className="relative w-90 max-w-[90%] h-60 lg:w-85 lg:h-50 overflow-hidden ">
+              <Image
+                src="/hotel-fachada.webp"
+                fill
+                priority
+                alt="img en página contacto"
+                className="object-cover"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-12">
+            <p>
+              Para garantizarte una atención profesional y segura, hemos
+              delegado la gestión de consultas y reservas en una agencia
+              especializada. Si quieres conocer más detalles sobre el
+              alojamiento o confirmar disponibilidad, haz clic en el siguiente
+              enlace para acceder a su portal oficial.
+            </p>
+            <div className="flex flex-col w-full gap-2">
+              <div className="flex flex-col w-full items-center justify-center gap-6 lg:gap-8">
+                <a className="border-2 border-green-950 bg-green-50 rounded-4xl cursor-pointer w-fit py-2 px-6 shadow-[4px_4px_0px_0px_rgba(5,46,7,1)] active:shadow-none active:translate-x-0.5 active:translate-y-0.5 hover:bg-green-100 transition-all flex items-center gap-2">
+                  {" "}
+                  Ver disponibilidad y consultas
+                  <FontAwesomeIcon icon={faArrowTrendUp} className="text-lg" />
+                </a>
+                <p className="text-sm text-center">
+                  (Serás redirigido al portal oficial de nuestra agencia
+                  afiliada.)
+                </p>
+              </div>
+              <div className="hidden lg:inline relative w-90 h-60 lg:w-85 lg:h-50 overflow-hidden">
+                <Image
+                  src="/hotel-fachada.webp"
+                  fill
+                  priority
+                  alt="img en página contacto"
+                  className="object-cover"
+                  sizes="(max-width: 768px) 600px, 1200px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
       <Footer />
     </>
   );
--- costera-n1/app/components/Footer.jsx
+++ costera-n1/app/components/Footer.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { useI18n } from "../hooks/usei18n";
 
 export const Footer = () => {
