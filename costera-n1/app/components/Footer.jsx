540f7bb356cc72e1acf351577d0dd1ee1c509f0b
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-04-22 11:39:17 +0200

    añadir props de cambio de color del footer

--- costera-n1/app/components/Footer.jsx
+++ costera-n1/app/components/Footer.jsx
@@ -1,23 +1,26 @@
 "use client";
 import { useI18n } from "../hooks/usei18n";
 
-export const Footer = () => {
+export const Footer = ({
+  bgColor = "bg-green-950",
+  textColor = "text-white",
+}) => {
   const { t } = useI18n();
 
   const textsFooter = t.Footer;
 
   return (
     <footer
-      className="relative h-[60vh] bg-green-950 w-full "
+      className={`relative h-[60vh] ${bgColor} w-full`}
       style={{ clipPath: "polygon(0% 0, 100% 0, 100% 100% , 0 100%)" }}
     >
       <div className="relative h-[calc(100vh+60vh)] -top-[100vh]">
         <div className="sticky top-[calc(100vh-40vh)] h-[40vh] flex flex-col items-center">
           <div className="flex flex-col gap-4">
-            <h4 className="text-5xl lg:text-9xl text-white font-bold">
+            <h4 className={`text-5xl lg:text-9xl ${textColor} font-bold`}>
               {textsFooter.title}
             </h4>
-            <p className="text-center text-white">{textsFooter.subtitle}</p>
+            <p className={`text-center ${textColor}`}>{textsFooter.subtitle}</p>
           </div>
         </div>
       </div>
