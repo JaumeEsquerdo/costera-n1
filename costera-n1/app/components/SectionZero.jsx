f784069a56602e9def975073057ddc26a55d574d
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-05-11 14:05:24 +0200

    ajustar bien el redondeo de la img en SectionZero

--- costera-n1/app/components/SectionZero.jsx
+++ costera-n1/app/components/SectionZero.jsx
@@ -61,9 +61,9 @@ export const SectionZero = () => {
           backfaceVisibility: "hidden", // Evita artefactos visuales
           perspective: 1000,
         }}
-        className="relative flex flex-col lg:flex-row items-center gap-4 lg:gap-6 pt-22 pb-6 lg:py-40 overflow-hidden rounded-2xl"
+        className="relative flex flex-col lg:flex-row items-center gap-4 lg:gap-6 pt-22 pb-6 lg:py-40 overflow-hidden"
       >
-        <motion.div className="relative w-40 h-70 lg:w-56 lg:h-80 overflow-hidden">
+        <motion.div className="relative w-40 h-70 lg:w-56 lg:h-80 overflow-hidden rounded-2xl">
           <MotionImage
             alt=""
             style={{ x: xImage, scale: 1.2 }}
